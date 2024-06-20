61c4f054f6b1cfcb400fc653c1e3f44a0fc47942
mjh1207 <mitchh1207@gmail.com>
2024-06-20 11:54:07 -0600

    Add Cell class and rename window
    Also add gitignore

---
+++ src/graphics.py
@@ -0,0 +1,74 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        self.__canvas.pack()
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("Window closed...")
+    
+    def close(self):
+        self.__running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+class Line:
+    def __init__(self, pointA, pointB):
+        self.pointA = pointA
+        self.pointB = pointB
+    
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.pointA.x, self.pointA.y, self.pointB.x, self.pointB.y, fill=fill_color, width=2
+        )
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(line)
+
+    
--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-from window import Window
+from graphics import Window, Point, Line, Cell
 
 def main():
     win = Window(800, 600)
+    
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(200, 300, 50, 100)
     win.wait_for_close()
 
 main()
