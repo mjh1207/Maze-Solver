2da74a748742d5bf5f45e798cb9cb600c9a413a7
mjh1207 <mitchh1207@gmail.com>
2024-07-08 12:00:41 -0600

    Maze class

--- src/graphics.py
+++ src/graphics.py
@@ -71,4 +71,8 @@ class Cell:
             line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             self._win.draw_line(line)
 
-    
+    def draw_move(self, to_cell, undo=False):
+        color = "red" if not undo else "gray"
+        cur_center = Point((self._x2 + self._x1) / 2, (self._y2 + self._y1) / 2)
+        dest_center = Point((to_cell._x2 + to_cell._x1) / 2, (to_cell._y2 + to_cell._y1) / 2)
+        self._win.draw_line(Line(cur_center, dest_center), color)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 from graphics import Window, Point, Line, Cell
+from maze import Maze
 
 def main():
     win = Window(800, 600)
     
-    cell = Cell(win)
-    cell.has_right_wall = False
-    cell.draw(200, 300, 50, 100)
+    # cellA = Cell(win)
+    # cellA.has_top_wall = False
+    # cellA.draw(405, 303, 505, 403)
+
+    # cellB = Cell(win)
+    # cellB.has_bottom_wall = False
+    # cellB.draw(405, 203, 505, 303)
+
+    # cellA.draw_move(cellB)
+
+    maze = Maze(50, 50, 3, 5, 10, 10, win)
+
     win.wait_for_close()
 
 main()
